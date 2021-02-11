0e0b5de6598e63d2d352a788a6e7dc15bb3bd2f2
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-11 16:55:47 +0200

    Add some class

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import Greeting from './components/component';
+import AlohaDashboard from './components/AlohaDash/index';
+import Aloha from './components/Aloha/index';
 
 
 class App extends Component {
-  render() {
-    const user = {
-      firstName: 'John',
-      url: 'http://localhost:3000/favicon.ico',
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          id: 1,
+          firstName: 'Neo',
+          surname: 'Andersen'
+        },
+        {
+          id: 2,
+          firstName: 'Tomas',
+          surname: 'Andersen'
+        },
+        {
+          id: 3,
+          firstName: 'Morpheus',
+          surname: 'Nav'
+        }, {
+          id: 4,
+          firstName: 'Trynity',
+          surname: 'Andersen'
+        }, {
+          id: 5,
+          firstName: 'Neoba',
+          surname: 'Andersen'
+        }
+      ]
     };
+  }
+  render() {
+const {users}=this.state;
     return (
-      <div>
-        <Greeting name={user.firstName} photo={user.url} />
-        <Greeting name='Neo' photo={user.url} />
-      </div>
+     <>
+     <AlohaDashboard users={users}/>
+     </>
     );
   }
 }
