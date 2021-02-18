46eb3a58a6caa8bbfaada1dec6ff2f1b38e6368a
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-18 15:02:15 +0200

    Add sass node install

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Counter from './components/Counter';
 import Calendar from './components/Calendar';
 
-function App() {
-  const stepNumber = 100;
+function App(props) {
+  
   return (
     <>
-      <Counter stepNumber={stepNumber} />
+      <Calendar/>
     </>
   );
 }
