3444f89da9ffa5eb348401adce0a7f4ed74c5bb0
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-19 15:45:41 +0200

    Add new example with children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Counter from './components/Counter';
 import Calendar from './components/Calendar';
+import FlexContainer from './components/FlexContainer';
 
 function App(props) {
   return (
     <>
-      <Calendar />
+      <FlexContainer jc='center' ai='center' fd="column">
+        <div>Div 1</div>
+        <div>Div 2</div>
+        <div>Div 3</div>
+        <div>Div 4</div>
+        <div>Div 5</div>
+        <div>Div 6</div>
+        <div>Div 7</div>
+        <div>Div 9</div>
+        <div>Div 10</div>
+      </FlexContainer>
     </>
   );
 }
---
+++ src/components/FlexContainer/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './FlexCon.module.scss';
+import PropTypes from 'prop-types';
+
+const FlexContainer = (props) => {
+    const { children, ai, jc, fd } = props;
+    const innerStyles = {
+        alignItems: ai,
+        justifyContent: jc,
+        flexDirection: fd,
+    };
+    return (
+        <div className={styles.container} style={innerStyles}>
+            {children}
+        </div>
+    );
+}
+
+FlexContainer.propTypes = {
+    children: PropTypes.element.isRequired,
+    ai: PropTypes.string.isRequired,
+    jc: PropTypes.string.isRequired,
+    fd: PropTypes.string.isRequired,
+}
+
+
+export default FlexContainer;
